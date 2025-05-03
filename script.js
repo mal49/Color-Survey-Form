8fc357887cb76d4f75199d3533a59dfc803e6905
Ikhmal Hanif <ikhmalhanif60@gmail.com>
2025-05-03 10:12:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const range = document.getElementById('range');
+
+range.addEventListener('input', (e) => {
+    const label = e.target.nextElementSibling;
+    const value = +e.target.value;
+    const range_width = getComputedStyle(e.target).getPropertyValue('width');
+    const label_width = getComputedStyle(label).getPropertyValue('width');
+
+    const num_width = +range_width.substring(0, range_width.length - 2);
+    const num_label_width = +label_width.substring(0, label_width.length - 2);
+
+    const max = +e.target.max;
+    const min = +e.target.min;
+
+    const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10);
+
+    label.style.left = `${left}px`;
+    label.innerHTML = Math.floor(value);
+});
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
+let btn = document.querySelector('.btn');
+    btn.addEventListener('click', function () {
+        Swal.fire({
+            title: 'Thank you!',
+            text: 'We got you response',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    });
+
